633496bba85db14bc442727b11bf6b762311db2c
Ali <aliali.ali1378@yahoo.com>
2021-09-30 15:44:57 +0330

    User Store Was Created

--- src/modules/User/store/index.js
+++ src/modules/User/store/index.js
@@ -1,12 +1,46 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
+const state = {
+  isAuthenticated:false,
+  token:""
+}
+
+const mutations = {
+  login(state, token) {
+    state.isAuthenticated = true
+    state.token = token
+    localStorage.setItem("token", token)
+    axios.defaults.headers.common['Authorization'] = "Bearer " + token
+  },
+  logout(state) {
+    state.isAuthenticated = false
+    state.token = ''
+    axios.defaults.headers.common['Authorization'] = ""
+    localStorage.removeItem("token")
+  },
+}
+
+const actions = {
+  onStart(context) {
+    let token = localStorage.getItem("token")
+    if (token) {
+      axios.defaults.headers.common['Authorization'] = "Token " + token
+      axios
+        .get('/api/auth/users/me/')
+        .then(response => {
+          context.commit('login', token)
+        })
+        .catch(error => {
+          context.commit('logout')
+        })
+    } else {
+      context.commit('logout')
+    }
+  }
+}
 
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  state,
+  mutations,
+  actions
 })
