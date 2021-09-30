bc11e3ebe62782ca0890afd7de40ca69e05e5c1a
Ali <aliali.ali1378@yahoo.com>
2021-09-30 14:48:59 +0330

    Manage Product Store, Actions and Getters

--- src/modules/Product/store/index.js
+++ src/modules/Product/store/index.js
@@ -1,12 +1,48 @@
 import { createStore } from 'vuex'
 
+const state = {
+  productItems:[],
+}
+const mutations ={
+  updateProductItems(state, payload) {
+    state.productItems = payload;
+  },
+}
+
+
+const actions= {
+  getProductItems ({ commit }) {
+    axios.get(`/api/products`).then((response) => {
+      commit('updateProductItems', response.data)
+    });
+  },
+}
+
+const getters ={
+  getProduct: state=> (code)=>{
+    return state.productItems.find(product=> product.code == code)
+  },
+  getProductByCategory: state=> (category)=>{
+    return state.productItems.filter(product=>product.category.name == category)
+  },
+  getOfferProducts: state=> {
+    return state.productItems.filter(product=> product.is_on_offer)
+  },
+  getSuperOfferProduct: state=>{
+    return state.productItems.find(product=> product.is_super_offer)
+  },
+
+}
+
+
+
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  state,
+  mutations,
+  actions,
+  modules: {},
+  getters
+
+ 
+  
 })
