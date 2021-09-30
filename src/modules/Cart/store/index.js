1c574517d7f7764820e4377226fe1a697387e367
Ali <aliali.ali1378@yahoo.com>
2021-09-30 15:01:12 +0330

    Cart Store was Created

--- src/modules/Cart/store/index.js
+++ src/modules/Cart/store/index.js
@@ -1,12 +1,58 @@
 import { createStore } from 'vuex'
+const state= {
+  cartItems:[]
+}
+const mutations= {
+  updateCartItems(state,payload){
+    state.cartItems = payload
+  },
+}
+const actions = {
+  getCartItems({ commit }) {
+    // axios.get('/api/cart').then((response) => {
+    //   commit('updateProductItems', response.data)
+    // });
+  },
+  addCartItem ({ commit }, cartItem) {
+    // axios.post('/api/cart', cartItem).then((response) => {
+    //   commit('updateProductItems', response.data)
+    // });
+  },
+  removeCartItem ({ commit }, cartItem) {
+    // axios.delete('/api/cart/delete', cartItem).then((response) => {
+    //   commit('updateProductItems', response.data)
+    // });
+  },
+  removeAllCartItems ({ commit }) {
+    // axios.delete('/api/cart/delete/all').then((response) => {
+    //   commit('updateProductItems', response.data)
+    // });
+  }
+}
+
+const getters = {
+  getCartItems: state => state.cartItems,
+  getCartTotalPrice: state=>{
+    let totalPrice = 0;
+    state.cartItems.reduce(item=>{
+      totalPrice += (item.price * item.quantity);
+    });
+    return totalPrice;
+  },
+  getCartQuantity: state=>{
+    let totalItems = 0;
+    state.cartItems.reduce(item=>{
+      totalItems += item.quantity;
+    });
+    return totalItems
+  }
+
+}
+
 
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+  state,
+  mutations,
+  actions,
+  getters
+});
