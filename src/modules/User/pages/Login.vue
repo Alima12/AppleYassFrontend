d9acc90bbafd613737436099afbca6ac5087e9ba
Ali <aliali.ali1378@yahoo.com>
2021-10-16 16:33:14 +0330

    Login validation

--- src/modules/User/pages/Login.vue
+++ src/modules/User/pages/Login.vue
@@ -2,15 +2,20 @@
   <div class="title">
     <p>ورود به حساب کاربری</p>
   </div>
-  <form action="" class="form">
+  <form action="" class="needs-validation" novalidate @submit.prevent="formvalid()">
     <div>
-      <label for="username"> نام کاربری:</label>
-      <input class="form-control" type="text" name="username"/>
+      <label for="username" class="form-label"> نام کاربری:</label>
+      <input class="form-control" :class="{'is-valid':usernameE==false,'is-invalid':usernameE==true}" type="text"  v-model="username" name="username" required/>
+      <div class="invalid-feedback">
+        {{usernameEM}}
+      </div>
     </div>
     <div>
-      <label for="password">رمز عبور:</label>
-      <input class="form-control" type="text" name="password"/>
-      
+      <label for="password" class="form-label" required>رمز عبور:</label>
+      <input class="form-control" :class="{'is-valid':passwordE==false,'is-invalid':passwordE==true}" type="password" name="password" v-model="password"/>
+      <div class="invalid-feedback">
+        {{passwordEM}}
+      </div>
     </div>
     <div class="link-to">
       <span><i class="fa fa-user"></i></span>
@@ -27,5 +32,48 @@
 <script>
   export default {
     name: "Login",
+    data(){
+      return{
+        username:"",
+        usernameE:null,
+        usernameEM:"",
+        password:"",
+        passwordE:null,
+        passwordEM:"", 
+      }
+    },
+    methods:{
+      formvalid(){
+        let english = /^[A-Za-z0-9]*$/;
+        this.usernameE = false;
+        this.usernameEM=""
+        if(this.username.length< 5){
+          this.usernameE = true;
+          this.usernameEM="نام کاربری شما باید بزرگتر از 4 کاراکتر باشد";
+        }else if(!english.test(this.username)){
+          this.usernameE = true;
+          this.usernameEM="نام کاربری شما میتواند تنها از حروف انگلیسی و اعداد انگلیسی استفاده تشکیل شود";
+        }
+
+        this.passwordE = false;
+        this.passwordEM="";
+
+        if(this.password.length< 8){
+          this.passwordE = true;
+          this.passwordEM=" رمز عبور شما باید بزرگتر از 8  کاراکتر باشد";
+        }
+      }
+    },
+    watch:{
+      username(){
+        this.usernameE = null;
+        this.usernameEM=""
+      },
+      password(){
+        this.passwordE = null;
+        this.passwordEM="";
+      }
+    }
+
   }
 </script>
