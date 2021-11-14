f4ada7e78a80f72f1fca0114d5bd521ee12a9f9f
Ali <aliali.ali1378@yahoo.com>
2021-11-14 12:00:16 +0330

    Manage address(add, remove) in profile

--- src/modules/Panel/pages/Profile.vue
+++ src/modules/Panel/pages/Profile.vue
@@ -1,16 +1,177 @@
 <template>
 
   <div class="main-content">
-    this is Profile Page
+    <form action="">
+       <div class="profile__info border cursor-pointer text-center">
+          <div class="avatar__img">
+            <img src="https://wac-cdn.atlassian.com/dam/jcr:ba03a215-2f45-40f5-8540-b2015223c918/Max-R_Headshot%20(1).jpg" class="avatar___img">
+            <input type="file" accept="image/*" class="hidden avatar-img__input">
+            <div class="v-dialog__container" style="display: block;"></div>
+            <div class="box__camera default__avatar"></div>
+          </div>
+          <span class="profile__name">کاربر : علی مهدوی</span>
+        </div>
+      <div class="row p-1">
+        <div class="col-lg-6 col-md-6 col-sm-12">  
+          <label for="fullname" class="mb-2">نام و نام خانوادگی:</label>
+          <input name="fullname" v-model="fullname" type="text" class="text font-size-13" placeholder="نام و نام خانوادگی">
+        </div>
+      </div>
+
+      <div class="row p-1">
+        <div class="col-lg-12 col-md-12 col-sm-12">  
+          <label for="email" class="mb-2">ایمیل:</label>
+          <input disabled name="email" v-model="email" type="text" class="text font-size-13 text-end" placeholder="ایمیل">
+        </div>
+      </div>
+      <div class="row p-1">
+        <div class="col-lg-10 col-md-8 col-sm-12">  
+          <label for="email" class="mb-2">شماره موبایل:</label>
+          <input name="email" v-model="phoneNumber" type="text" class="text font-size-13 text-end" placeholder="شماره موبایل">
+          <button class="btn btn-info" v-if="!changeMode" @click.prevent="changeNumber()">عوض کردن</button>
+          <button class="btn btn-warning" v-else>تایید</button>
+
+        </div>
+        <div class="col-lg-2 col-md-4 col-sm-12" v-if="changeMode">  
+          <label for="email" class="mb-2">کد:</label>
+          <input name="email" v-model="phoneNumber" type="text" class="text font-size-13 text-end" placeholder="کد تایید">
+        </div>
+      </div>
+
+
+
+      <div class="row p-1 my-3">
+        <div class="col-lg-12 col-md-12 col-sm-12">
+          <div class="current-address">
+            <h4>آدرس ها</h4>
+            <p class="user-address" v-for="address in user.address">
+              <span>
+                {{address}}
+              </span>
+              <i class="fa fa-trash fa-2x text-danger" @click="deleteAddress(address)"></i>
+              
+            </p>
+          </div> 
+          <button class="btn btn-success" @click.prevent="addAdress()">اضافه کردن</button>
+        </div>
+      </div>
+
+    </form>
   </div>
 </template>
 <script>
+  import Swal from 'sweetalert2'
   export default {
     name: "Profile",
     components:{},
-    created(){
-       
+    data(){
+      return{
+        fullname:"",
+        changeMode:false,
+        user:{
+          address:[
+            "فارس/شیراز/گلستان/خیابان رایحه/مجتمع فرهنگیان/بلوک 2/ طبقه اول /واحد 412",
+            "کهگیلویه و بویر احمد/دهدشت/گلزار شهدا/گلزار هشتم"
+          ]
+        },
+        newAddress:''
+      }
     },
+    methods:{
+      changeNumber(){
+        this.changeMode = true;
+      },
+      deleteAddress(address){
+        Swal.fire({
+          title: '<h5>حذف آدرس</h5>',
+          html: `<p class="text-danger">آیا از حذف آدرس زیر اطمینان دارید؟</p>
+          <p class="fw-bold">${address}</p>`,
+          icon: 'warning',
+          confirmButtonText: 'حذف کن',
+          confirmButtonColor:"#e74c3c",
+          CancelButtonColor:"#95a5a6",
+          cancelButtonText:"انصراف",
+          confirmButtonText: 'حذف',
+          showCancelButton: true,
+        }).then(result=>{
+          if (result.isConfirmed) {
+              this.user.address = this.user.address.filter(ad=> ad != address);
+              Swal.fire({
+                  title: 'حذف با موفقیت انجام شد',
+                  text:'حذف آدرس با موفقیت انجام شد',
+                  icon:'success',
+                  confirmButtonColor:"#27ae60",
+                  confirmButtonText: 'متوجه شدم',
+              })
+          }
+        });
+      },
+      addAdress(){
+        Swal.fire({
+          title: '<h5>افزودن آدرس</h5>',
+          html: `<input type="text" name="addresstoadd" v-model="newAddress" class="form-control new-address-form" />`,
+          icon: 'info',
+          confirmButtonText: 'اضافه کن',
+          confirmButtonColor:"#009432",
+          CancelButtonColor:"#95a5a6",
+          cancelButtonText:"انصراف",
+          showCancelButton: true,
+        }).then(result=>{
+          this.newAddress = document.querySelector(".new-address-form").value
+          if (result.isConfirmed && this.newAddress != '') {
+
+              this.user.address.push(this.newAddress);
+              Swal.fire({
+                  title: 'موفق',
+                  text:'آدرس با موفقیت اضافه شد',
+                  icon:'success',
+                  confirmButtonColor:"#27ae60",
+                  confirmButtonText: 'متوجه شدم',
+              })
+              this.newAddress = "";
+          }
+        });
+      }
+      
+    }
+
   }
 
 </script>
+
+<style >
+.current-address{
+  margin: 5px 10px;
+}
+
+.current-address h4{
+  color: #333;
+  font-weight: bold;
+  padding-bottom: 5px;
+  margin-bottom: 15px;
+
+  border-bottom: 2px solid #d63031 ;
+
+}
+
+.current-address .user-address{
+  padding: 5px 10px;
+  margin: 10px 0px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border: 1px outset #777;
+  border-radius: 10px;
+}
+
+
+.current-address .user-address span{
+  font-weight: bold;
+  font-size: .8em;
+
+}
+.current-address .user-address i{
+  cursor: pointer;
+  
+}
+</style>
