7433a1fed632eca1d47d137e408e310ae4e627f7
Ali <aliali.ali1378@yahoo.com>
2021-11-23 21:36:09 +0330

    Admin permission limited

--- src/modules/Panel/components/SideBar.vue
+++ src/modules/Panel/components/SideBar.vue
@@ -1,7 +1,7 @@
 <template>
   <ul>
         <li
-          v-for="route in panelRoutes"
+          v-for="route in filterdpanelRoutes()"
           class="item-li"
           :class="setClass(route)"
         >
@@ -15,37 +15,40 @@
     name: "SideBar",
     data(){
       return{
+        user:{isAdmin:false},
         panelRoutes:[
-          {path:"/panel/dashboard",title:"داشبورد",icon:"i-dashboard"},
-          {path:"/panel/myorders",title:"سفارشات من",icon:"i-orders"},
-          {path:"/panel/mytransactions",title:"تراکنش های من",icon:"i-transactions"},
-          {path:"/panel/category",title:"دسته بندی",icon:"i-categories"},
-          {path:"/panel/profile",title:"پروفایل",icon:"i-user__inforamtion"},
+          {adminPermission:false,path:"/panel/dashboard",title:"داشبورد",icon:"i-dashboard"},
+          {adminPermission:false,path:"/panel/myorders",title:"سفارشات من",icon:"i-orders"},
+          {adminPermission:false,path:"/panel/mytransactions",title:"تراکنش های من",icon:"i-transactions"},
+          {adminPermission:false,path:"/panel/profile",title:"پروفایل",icon:"i-user__inforamtion"},
 
 
-          {path:"/panel/users",title:"کاربران",icon:"i-users"},
-          {path:"/panel/slideshow",title:"مدیریت اسلاید",icon:"i-slideshow"},
-          {path:"/panel/products",title:"اجناس",icon:"i-basket"},
-          {path:"/panel/charts",title:"آمار",icon:"i-chart"},
-          {path:"/panel/discount",title:"تخفیف",icon:"i-discounts"},
-          {path:"/panel/comments",title:"نظرات",icon:"i-comments"},
-          {path:"/panel/transactions",title:"تراکنش ها",icon:"i-transactions"},
-          {path:"/panel/orders",title:"سفارشات",icon:"i-orders"},
-          {path:"/panel/setting",title:"تنظیمات",icon:"i-setting"},
+          {adminPermission:true,path:"/panel/users",title:"کاربران",icon:"i-users"},
+          {adminPermission:true,path:"/panel/category",title:"دسته بندی",icon:"i-categories"},
+          {adminPermission:true,path:"/panel/slideshow",title:"مدیریت اسلاید",icon:"i-slideshow"},
+          {adminPermission:true,path:"/panel/products",title:"اجناس",icon:"i-basket"},
+          {adminPermission:true,path:"/panel/charts",title:"آمار",icon:"i-chart"},
+          {adminPermission:true,path:"/panel/discount",title:"تخفیف",icon:"i-discounts"},
+          {adminPermission:true,path:"/panel/comments",title:"نظرات",icon:"i-comments"},
+          {adminPermission:true,path:"/panel/transactions",title:"تراکنش ها",icon:"i-transactions"},
+          {adminPermission:true,path:"/panel/orders",title:"سفارشات",icon:"i-orders"},
+          {adminPermission:true,path:"/panel/setting",title:"تنظیمات",icon:"i-setting"}
           
-
-
-
-
         ]
       }
     },
     methods:{
       setClass(route){
-        console.log(this.$route.path)
         let cls = `${route.icon}`
         cls += route.path == this.$route.path ? ` is-active` : ''
         return cls
+      },
+      filterdpanelRoutes(){
+        return this.panelRoutes.filter(route=>{
+          if(!route.adminPermission || route.adminPermission && this.user.isAdmin){
+            return route
+          }
+        })
       }
     }
   }
